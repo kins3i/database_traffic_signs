21aaa3ee053fce3b79f90253f9bc75c07bc0c09c
kins3i <natkow97@gmail.com>
2024-01-01 22:25:53 +0100

    added resizing, added rescaling from 0-255 to 0-1
    TODO: correct data augmentation, divide data for train and validation datasets, create model and train using layers

--- neural_network_project.m
+++ neural_network_project.m
@@ -1,12 +1,41 @@
+% clear; 
+clc;
 imds = imageDatastore("images", ...
     'IncludeSubfolders',true, ...
     'LabelSource','foldernames');
+
+img_imds1 = read(imds);
+
+whos img_imds1
 
 % TODO:
 % 1) zmienić rozmiar na 32x32
 % 2) augmentacja danych według kodu w kaggle (bez obracania!)
 
 
+% TODO 1:
+targetSize = [32 32];
+
+imds_resized = transform(imds,@(x) imresize(x,targetSize));
+imds_resized = transform(imds_resized,@(x) im2double(x));
+
+img_resized1 = read(imds_resized);
+whos img_resized1
+
+
+% TODO 2:
+% https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator
+% train_image_generator_aug = ImageDataGenerator(rescale=1./255,
+%                                            shear_range=0.1,
+%                                            zoom_range=0.2,
+%                                            brightness_range=[0.5, 1.5],
+%                                            width_shift_range=0.2,
+%                                            height_shift_range=0.2,
+%                                            channel_shift_range=0.2)
+augmenter = imageDataAugmenter( ...
+    'RandScale',[0.5 1], ...
+    'RandXShear', [0, 0], ...
+    'RandYShear', [0, 0]  );
 
 
 
